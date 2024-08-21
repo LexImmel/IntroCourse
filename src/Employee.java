0c5cfd587440f362af4e313011b19573e358a2f0
LexImmel <juffin91@gmail.com>
2024-08-22 00:33:49 +0500

    Initial basic commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,10 +1,13 @@
+import java.util.Objects;
+
 public final class Employee {
+    private static int counter = 0;
     private final String name;
     private final String secondName;
     private final String surname;
-    private final int department;
+    private int department;
     private int salary;
-    private static int id;
+    private int id;
 
 
     public Employee(String surname, String name, String secondName, int department, int salary) {
@@ -13,38 +16,54 @@ public final class Employee {
         this.surname = surname;
         this.department = department;
         this.salary = salary;
-        this.id = 1;
+        this.id = ++counter;
     }
 
-    public String getName() {
-        return name;
-    }
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public String getSurname() {
-        return surname;
+    public String getFullName() {
+        return surname + " " + name + " " + secondName;
     }
 
     public int getDepartment() {
         return department;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
 
     public int getId() {
-        return id++;
+        return id;
     }
 
+    public void setDepartment(int department) {
+        if (department >= 1 && department <= 5) {
+            this.department = department;
+        } else {
+            throw new IllegalArgumentException("Отделов только пять.");
+        }
+    }
+
+    @Override
     public String toString() {
-        return "Фамилия сотрудника: " + surname + " Имя: " + name + " Отчество "+ secondName + " Код отдела: " + department + " Зарплата: " + salary + " Id: " + getId();
+        return "Фамилия сотрудника: " + surname + " Имя: " + name + " Отчество: " + secondName + " Код отдела: " + department + " Зарплата: " + salary + " Id: " + getId();
     }
 
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Employee employee = (Employee) object;
+        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(surname, secondName, name, department, salary, id);
+    }
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
+
+    public static Employee createEmployee(String name, String surname, String secondName, int department, int salary) {
+        Employee employee = new Employee(name, surname, secondName, department, salary);
+        return employee;
+    }
+
+    private static  Employee[] emp = new Employee[10];
+
     public static void main(String[] args) {
-        Employee[] emp = new Employee[10];
+
         emp[0] = new Employee("Волков", "Игорь", "Петрович", 1, 200_000);
         emp[1] = new Employee("Драник", "Влад", "Константинович", 2, 150_000);
         emp[2] = new Employee("Фрай", "Макс", "Филиппович", 3, 280_000);
@@ -11,43 +19,172 @@ public class Main {
         emp[7] = new Employee("Этвуд", "Мария", "Вадимовна", 3, 245_000);
         emp[8] = new Employee("Веркин", "Эдуард", "Владимирович", 4, 155_000);
         emp[9] = new Employee("Горох", "Надежда", "Яковлевна", 5, 200_000);
+
         System.out.println("Основная часть.");
-//Показать все данные сотрудников компании:
+
+        printAllEmployees();
+        int total = calculateSumSalary();
+        System.out.println("Общая сумма зарплат: " + total);
+        Employee minSalaryEmployee = findEmployeeMinSalary();
+        System.out.println("Сотрудник с минимальной зарплатой: " + minSalaryEmployee);
+        Employee maxSalaryEmployee = findEmployeeMaxSalary();
+        System.out.println("Сотрудник с максимальной зарплатой: " + maxSalaryEmployee);
+        double avgSalary = calculateAvgSalary();
+        System.out.println("Среднее значение зарплат: " + avgSalary);
+        printEmployeeFullNames();
+        System.out.println(" ");
+
+        System.out.println("Повышенная сложность.");
+        int setPercent = 15; //Процент для подсчета ЗП
+        int setDept = 1; //Номер департамента для подсчетов
+        countIndexedSalary(setPercent);//Подсчет относительно указанного процента
+        printAllEmployees();
+        //Все данные ниже можно увидеть о каждом отделе, указав сверху номер отдела
+        System.out.println("Зарплата отдела " + setDept + " равна: " + salaryOfDept(setDept));
+        System.out.println("Минимальная зп в отделе " + setDept + " равна: " + minSalaryOfDept(setDept));
+        System.out.println("Максимальная зп в отделе " + setDept + " равна: " + maxSalaryOfDept(setDept));
+        countIndSalaryDept(setDept, setPercent);
+        System.out.println("Проиндексированная зп в отделе: ");
+        printDeptEmployees(setDept);
+        int lowestSalary = 180_000;
+        int highestSalary = 280_000;
+        System.out.println("У указанных сотрудников зарплата ниже " + lowestSalary);
+        salaryLesserThan(lowestSalary);
+        System.out.println("У указанных сотрудников зарплата выше или равна " + highestSalary);
+        salaryHigherThan(highestSalary);
+    }
+
+    //Показать все данные сотрудников компании:
+    public static void printAllEmployees() {
         for (Employee employee : emp) {
-            System.out.println(employee.toString());
+            System.out.println(employee);
         }
-//Подсчет общей суммы зарплат
-        int sum = 0;
-        for (Employee value : emp) {
-            sum = sum + value.getSalary();
-        }
-        System.out.println("Траты на зарплату в месяц: " + sum);
-//Вывод минимальной и максимальной зарплат
-        int minSalary = emp[0].getSalary();
-        int maxSalary = minSalary;
+    }
+
+    //Показать данные по департаменту
+    public static void printDeptEmployees(int dept) {
+        System.out.println("Отдел номер " + dept + ":");
         for (Employee employee : emp) {
-            if (employee.getSalary() < minSalary) {
-                minSalary = employee.getSalary();
-            } else if (employee.getSalary() > maxSalary) {
-                maxSalary = employee.getSalary();
+            if (employee.getDepartment() == dept) {
+                System.out.println(" ФИО сотрудника: " + employee.getFullName() + " Зарплата: " + employee.getSalary() + " Id: " + employee.getId());
             }
         }
-        System.out.println("Минимальная зарплата в компании: " + minSalary);
-        System.out.println("Максимальная зарплата в компании: " + maxSalary);
-//Вывод средней зарплаты
-        int avgSalary = sum / emp.length;
-        System.out.println("Средняя зарплата в компании: " + avgSalary);
-//Вывод ФИО всех сотрудников
-        for (Employee employee : emp) {
-            System.out.println(employee.getSurname() + " " + employee.getName() + " " + employee.getSecondName());
-        }
-        //////////////////////////////////////////////////////////////////////////
-        System.out.println("Повышенная сложность.");
-        //Индексация зарплаты на фиксированный процент
-        for (Employee employee : emp) {
-            employee.setSalary((int) (employee.getSalary() * 1.15));
-            System.out.println(employee.toString());
-        }
-
     }
+
+    //Подсчет общей суммы зарплат
+    public static int calculateSumSalary() {
+        int total = 0;
+        for (Employee employee : emp) {
+            total += employee.getSalary();
+        }
+        return total;
+    }
+
+    //Вывод минимальной и максимальной зарплат
+    public static Employee findEmployeeMinSalary() {
+        if (emp.length == 0) return null;
+        Employee minSalaryEmployee = emp[0];
+        for (int i = 1; i < emp.length; i++) {
+            if (emp[i].getSalary() < minSalaryEmployee.getSalary()) minSalaryEmployee = emp[i];
+        }
+        return minSalaryEmployee;
+    }
+
+    public static Employee findEmployeeMaxSalary() {
+        if (emp.length == 0) return null;
+        Employee maxSalaryEmployee = emp[0];
+        for (int i = 1; i < emp.length; i++) {
+            if (emp[i].getSalary() > maxSalaryEmployee.getSalary()) maxSalaryEmployee = emp[i];
+        }
+        return maxSalaryEmployee;
+    }
+
+    //Вывод средней зарплаты
+    public static double calculateAvgSalary() {
+        if (emp.length == 0) return 0;
+        return (double) calculateSumSalary() / emp.length;
+    }
+
+    //Вывод ФИО всех сотрудников
+    public static void printEmployeeFullNames() {
+        for (Employee employee : emp) {
+            System.out.println(employee.getFullName());
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    //Индексация зарплаты на фиксированный процент
+    public static void countIndexedSalary(int percent) {
+        for (Employee employee : emp) {
+            int currentSalary = employee.getSalary();
+            employee.setSalary((int) (currentSalary * (percent / 100f + 1)));
+        }
+    }
+
+    //Зп отдела
+    public static int salaryOfDept(int dept) {
+        int sum = 0;
+        for (Employee employee : emp) {
+            if (employee.getDepartment() == dept) {
+                sum += employee.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    //Мин зп в отделе
+    public static int minSalaryOfDept(int dept) {
+        int minSalaryDept = emp[0].getSalary();
+        for (Employee employee : emp) {
+            if (employee.getDepartment() != dept) {
+                continue;
+            }
+            if (employee.getSalary() < minSalaryDept) {
+                minSalaryDept = employee.getSalary();
+            }
+        }
+        return minSalaryDept;
+    }
+
+    //Макс зп в отделе
+    public static int maxSalaryOfDept(int dept) {
+        int maxSalaryDept = emp[0].getSalary();
+        for (Employee employee : emp) {
+            if (employee.getDepartment() != dept) {
+                continue;
+            }
+            if (employee.getSalary() > maxSalaryDept) {
+                maxSalaryDept = employee.getSalary();
+            }
+        }
+        return maxSalaryDept;
+    }
+
+    //Проиндексированная зп в указанном отделе на указанный процент
+    public static void countIndSalaryDept(int dept, int percent) {
+        for (Employee employee : emp) {
+            int currentSalary = employee.getSalary();
+            if (employee.getDepartment() == dept) {
+                employee.setSalary((int) (currentSalary * (percent / 100f + 1)));
+
+            }
+        }
+    }
+
+    public static void salaryLesserThan(int lowestSalary) {
+        for (Employee employee : emp) {
+            if (employee.getSalary() < lowestSalary) {
+                System.out.println(" ФИО сотрудника: " + employee.getFullName() + " Зарплата: " + employee.getSalary() + " Id: " + employee.getId());
+            }
+        }
+    }
+
+    public static void salaryHigherThan(int highestSalary) {
+        for (Employee employee : emp) {
+            if (employee.getSalary() >= highestSalary) {
+                System.out.println(" ФИО сотрудника: " + employee.getFullName() + " Зарплата: " + employee.getSalary() + " Id: " + employee.getId());
+            }
+        }
+    }
+
 }
